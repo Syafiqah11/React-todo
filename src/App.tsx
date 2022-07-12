080484a9cf18ecdaec9207fdcba9582a6521b68f
Ikaa <nurul.syafiqah@besquare.com.my>
2022-07-12 13:57:24 +0800

    update commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,114 +2,148 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-const App = () => {
-  const [name, setName] = React.useState("");
-  const [age, setAge] = React.useState("");
-  const [fav_color, setFavColor] = React.useState("");
-
-  const onSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-  };
-
-  const clear = () => {
-    setName("");
-    setAge("");
-    setFavColor("");
-  };
-
+function App() {
   return (
-    <div className="pa-4">
+    <div className="App">
       <div className="d-flex v-center h-center">
         <div className="w-50">
-          <h2>Fill the Form!</h2>
-          <form onSubmit={onSubmit}>
-            <div className="pa-1">
+          <div>
+            <h2>Fill the form</h2>
+            <form>
               <div>
                 <label>Name</label>
+                <input type="text" placeholder="Enter name"></input>
               </div>
-              <input
-                className="my-input full-width"
-                type="text"
-                name="name"
-                placeholder="Enter Name"
-              />
-            </div>
-
-            <div className="pa-1">
+              <div>
+                <label>Gender</label>
+                <div className="radio-buttons"></div>
+                <input type="radio" name="gender" value="male"></input>
+                <input type="radio" name="gender" value="female"></input>
+              </div>
               <div>
                 <label>Age</label>
+                <input type="text" placeholder="Enter age"></input>
               </div>
-              <input
-                className="my-input full-width"
-                type="text"
-                name="age"
-                placeholder="Age"
-              />
-            </div>
-
-            <div className="pa-1">
               <div>
-                <label>Favourite color</label>
+                <label>favourite color</label>
+                <select>
+                  <option>red</option>
+                  <option>green</option>
+                  <option>blue</option>
+                  <option>yellow</option>
+                  <option>orange</option>
+                  <option>pink</option>
+                  <option>cyan</option>
+                  <option>grey</option>
+                  <option>black</option>
+                  <option>white</option>
+                  <option>skyblue</option>
+                </select>
               </div>
-              <select name="fav_color" className="my-input full-width">
-                {[
-                  "red",
-                  "green",
-                  "blue",
-                  "yellow",
-                  "orange",
-                  "pink",
-                  "cyan",
-                  "grey",
-                  "black",
-                  "white",
-                  "skyblue",
-                ].map((color, index) => (
-                  <option value={color} key={color + index}>
-                    {color}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div className="pa-1">
-              <button type="submit" className="primary full-width">
-                Submit
-              </button>
-              <button onClick={clear} className="secondary full-width">
-                Clear
-              </button>
-            </div>
-          </form>
-        </div>
-        <div className="w-50 pa-4">
-          <p className="special-text">
-            Sandeep is 29 years old, and he likes Skyblue color.
-          </p>
+              <div>
+                <button type="submit">Submit</button>
+              </div>
+              <div>
+                <button>Clear</button>
+              </div>
+            </form>
+          </div>
+          <div className="w-50 pa-4">
+            <p>John is 26 years old, and he likes Skyblue color</p>
+          </div>
         </div>
       </div>
     </div>
   );
-};
+}
+
+// const App = () => {
+//   const [name, setName] = React.useState("");
+//   const [age, setAge] = React.useState("");
+//   const [fav_color, setFavColor] = React.useState("");
+
+//   const onSubmit = (e: React.FormEvent) => {
+//     e.preventDefault();
+//   };
+
+//   const clear = () => {
+//     setName("");
+//     setAge("");
+//     setFavColor("");
+//   };
+
+//   return (
+//     <div className="pa-4">
+//       <div className="d-flex v-center h-center">
+//         <div className="w-50">
+//           <h2>Fill the Form!</h2>
+//           <form onSubmit={onSubmit}>
+//             <div className="pa-1">
+//               <div>
+//                 <label>Name</label>
+//               </div>
+//               <input
+//                 className="my-input full-width"
+//                 type="text"
+//                 name="name"
+//                 placeholder="Enter Name"
+//               />
+//             </div>
+
+//             <div className="pa-1">
+//               <div>
+//                 <label>Age</label>
+//               </div>
+//               <input
+//                 className="my-input full-width"
+//                 type="text"
+//                 name="age"
+//                 placeholder="Age"
+//               />
+//             </div>
+
+//             <div className="pa-1">
+//               <div>
+//                 <label>Favourite color</label>
+//               </div>
+//               <select name="fav_color" className="my-input full-width">
+//                 {[
+//                   "red",
+//                   "green",
+//                   "blue",
+//                   "yellow",
+//                   "orange",
+//                   "pink",
+//                   "cyan",
+//                   "grey",
+//                   "black",
+//                   "white",
+//                   "skyblue",
+//                 ].map((color, index) => (
+//                   <option value={color} key={color + index}>
+//                     {color}
+//                   </option>
+//                 ))}
+//               </select>
+//             </div>
+//             <div className="pa-1">
+//               <button type="submit" className="primary full-width">
+//                 Submit
+//               </button>
+//               <button onClick={clear} className="secondary full-width">
+//                 Clear
+//               </button>
+//             </div>
+//           </form>
+//         </div>
+//         <div className="w-50 pa-4">
+//           <p className="special-text">
+//             Sandeep is 29 years old, and he likes Skyblue color.
+//           </p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
 export default App;
